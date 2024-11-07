5eedb36bde2fa728d8a447706b648dbbe5c933e4
Visualizeit <61679509+Visualizeit@users.noreply.github.com>
2024-11-07 14:18:07 +0800

    Initial commit

---
+++ src/Providers.tsx
@@ -0,0 +1,17 @@
+import { MantineProvider } from '@mantine/core'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { type PropsWithChildren } from 'react'
+import mantineTheme from './configs/mantineTheme'
+import queryClient from './configs/queryClient'
+
+import '@mantine/core/styles.css'
+
+const Providers = ({ children }: PropsWithChildren) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<MantineProvider theme={mantineTheme}>{children}</MantineProvider>
+		</QueryClientProvider>
+	)
+}
+
+export default Providers
---
+++ src/configs/mantineTheme.ts
@@ -0,0 +1,35 @@
+import {
+	type DefaultMantineColor,
+	type MantineColorsTuple,
+	createTheme,
+} from '@mantine/core'
+
+type ExtendedCustomColors = 'primary' | DefaultMantineColor
+
+declare module '@mantine/core' {
+	export interface MantineThemeColorsOverride {
+		colors: Record<ExtendedCustomColors, MantineColorsTuple>
+	}
+}
+
+const mantineTheme = createTheme({
+	primaryColor: 'primary',
+	defaultRadius: 'md',
+	activeClassName: 'active:brightness-90',
+	colors: {
+		primary: [
+			'#e5f4ff',
+			'#cde2ff',
+			'#9bc2ff',
+			'#64a0ff',
+			'#3984fe',
+			'#1d72fe',
+			'#0969ff',
+			'#0058e4',
+			'#004ecc',
+			'#0043b5',
+		],
+	},
+})
+
+export default mantineTheme
