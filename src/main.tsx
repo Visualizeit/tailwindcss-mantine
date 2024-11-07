78d0c2badcdb41024717032643aac2ce4b714aa1
Visualizeit <vnyeit@gmail.com>
2024-11-07 17:21:37 +0800

    Remove unused files and configurations, including Caddyfile, queryClient, and layout components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { RouterProvider } from '@tanstack/react-router'
+import { MantineProvider } from '@mantine/core'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import router from './configs/router'
-import Providers from './Providers'
+import mantineTheme from './configs/mantineTheme'
 
+import '@mantine/core/styles.css'
 import './index.css'
 
 const container = document.getElementById('root')
@@ -11,9 +11,9 @@ const container = document.getElementById('root')
 if (container) {
 	ReactDOM.createRoot(container).render(
 		<React.StrictMode>
-			<Providers>
-				<RouterProvider router={router} />
-			</Providers>
+			<MantineProvider theme={mantineTheme}>
+				<h1>Hello World</h1>
+			</MantineProvider>
 		</React.StrictMode>,
 	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		tsconfigPaths(),
-		TanStackRouterVite({ indexToken: 'page' }),
-		react(),
-	],
+	plugins: [tsconfigPaths(), react()],
 	server: {
 		open: true,
 	},
