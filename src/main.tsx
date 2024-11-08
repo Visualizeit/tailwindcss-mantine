e44d92ebacd099bf95796db625d70def612cab06
Visualizeit <17688172112@163.com>
2024-11-08 21:44:44 +0800

    Update heading class names for consistency in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ if (container) {
 	ReactDOM.createRoot(container).render(
 		<React.StrictMode>
 			<MantineProvider theme={mantineTheme}>
-				<h1 className="mt-heading1 text-mt-primary-filled">Hello World</h1>
+				<h1 className="m-heading1 text-m-primary-filled">Hello World</h1>
 			</MantineProvider>
 		</React.StrictMode>,
 	)
