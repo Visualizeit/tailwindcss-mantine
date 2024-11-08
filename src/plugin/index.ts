a318c17f11b3279d647336aef23eb9a9eba56108
Visualizeit <vnyeit@gmail.com>
2024-11-08 18:02:31 +0800

    Refactor Tailwind CSS plugin to streamline theme configuration and enhance variable resolution

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -2,6 +2,7 @@ import {
 	type MantineThemeOverride,
 	mergeMantineTheme,
 	DEFAULT_THEME,
+	defaultCssVariablesResolver,
 } from '@mantine/core'
 import plugin from 'tailwindcss/plugin'
 import { type CSSRuleObject, type KeyValuePair } from 'tailwindcss/types/config'
@@ -10,7 +11,7 @@ interface PluginOptions {
 	themeOverride?: MantineThemeOverride
 }
 
-const prefix = 'mt'
+const prefix = 'm'
 
 export default plugin.withOptions<PluginOptions>(
 	(options = {}) => {
@@ -21,9 +22,9 @@ export default plugin.withOptions<PluginOptions>(
 
 			// Headings
 			for (const size in theme.headings.sizes) {
-				const heading = size.replace('h', 'heading')
+				const tokenName = `${prefix}-${size.replace('h', 'heading')}`
 
-				components[`.${prefix}-${heading}`] = {
+				components[`.${tokenName}`] = {
 					fontSize: `var(--mantine-${size}-font-size)`,
 					lineHeight: `var(--mantine-${size}-line-height)`,
 					fontWeight: `var(--mantine-${size}-font-weight)`,
@@ -36,175 +37,130 @@ export default plugin.withOptions<PluginOptions>(
 	(options) => {
 		const theme = mergeMantineTheme(DEFAULT_THEME, options.themeOverride)
 
-		// Cursor
-		const cursor: KeyValuePair = {
-			[prefix]: 'var(--mantine-cursor-type)',
+		const resolvedVariables = defaultCssVariablesResolver(theme)
+
+		const themeConfig = {
+			scale: {} as KeyValuePair,
+			cursor: {} as KeyValuePair,
+			fontFamily: {} as KeyValuePair,
+			fontSize: {} as KeyValuePair,
+			lineHeight: {} as KeyValuePair,
+			fontWeight: {} as KeyValuePair,
+			colors: {} as KeyValuePair,
+			spacing: {} as KeyValuePair,
+			screens: {} as KeyValuePair,
+			borderRadius: {} as KeyValuePair,
+			boxShadow: {} as KeyValuePair,
+			zIndex: {} as KeyValuePair,
 		}
-
-		// Font family
-		const fontFamily = {
-			[prefix]: 'var(--mantine-font-family)',
-			[`${prefix}-monospace`]: 'var(--mantine-font-family-monospace)',
-			[`${prefix}-headings`]: 'var(--mantine-font-family-headings)',
-		}
-
-		// Font size
-		const fontSize: KeyValuePair = {}
-
-		for (const size in theme.fontSizes) {
-			fontSize[`${prefix}-${size}`] = `var(--mantine-font-size-${size})`
-		}
-
-		// Line height
-		const lineHeight: KeyValuePair = {
-			[prefix]: 'var(--mantine-line-height)',
-		}
-
-		for (const key in theme.lineHeights) {
-			lineHeight[`${prefix}-${key}`] = `var(--mantine-line-height-${key})`
-		}
-
-		// Font weight
-		const fontWeight: KeyValuePair = {
-			[`${prefix}-heading`]: 'var(--mantine-heading-font-weight)',
-		}
-
-		// Headings
-		for (const size in theme.headings.sizes) {
-			const heading = size.replace('h', 'heading')
-
-			fontSize[`${prefix}-${heading}`] = `var(--mantine-${size}-font-size)`
-			lineHeight[`${prefix}-${heading}`] = `var(--mantine-${size}-line-height)`
-			fontWeight[`${prefix}-${heading}`] = `var(--mantine-${size}-font-weight)`
-		}
-
-		// Colors variables
-		const colors: Record<string, string> = {}
 
 		{
-			for (const colorName in theme.colors) {
-				for (let shade = 0; shade < 10; shade++) {
-					colors[`${prefix}-${colorName}-${shade}`] =
-						`var(--mantine-color-${colorName}-${shade})`
+			// headings
+			themeConfig.fontWeight['heading'] = '--mantine-heading-font-weight'
+
+			for (const size in theme.headings.sizes) {
+				const tokenName = `${prefix}-${size.replace('h', 'heading')}`
+
+				themeConfig.fontSize[tokenName] = `var(--mantine-${size}-font-size)`
+				themeConfig.lineHeight[tokenName] = `var(--mantine-${size}-line-height)`
+				themeConfig.fontWeight[tokenName] = `var(--mantine-${size}-font-weight)`
+			}
+		}
+
+		{
+			// z-index
+			const zIndexLevels = ['app', 'modal', 'popover', 'overlay', 'max']
+
+			for (const level of zIndexLevels) {
+				themeConfig.zIndex[`${prefix}-${level}`] =
+					`var(--mantine-z-index-${level})`
+			}
+		}
+
+		interface TokenNameConfig {
+			tokens: Record<string, string>
+			searchValue: string
+			replaceValue?: string
+		}
+
+		const tokenNameConfigs: TokenNameConfig[] = [
+			{
+				tokens: themeConfig.scale,
+				searchValue: '--mantine-scale',
+			},
+			{
+				tokens: themeConfig.cursor,
+				searchValue: '--mantine-cursor-type',
+			},
+			{
+				tokens: themeConfig.fontFamily,
+				searchValue: '--mantine-font-family',
+			},
+			{
+				tokens: themeConfig.fontSize,
+				searchValue: '--mantine-font-size',
+			},
+			{
+				tokens: themeConfig.lineHeight,
+				searchValue: '--mantine-line-height',
+			},
+			{
+				tokens: themeConfig.colors,
+				searchValue: '--mantine-primary-color',
+				replaceValue: '-primary',
+			},
+			{ tokens: {}, searchValue: '--mantine-color-scheme' },
+			{
+				tokens: themeConfig.colors,
+				searchValue: '--mantine-color',
+			},
+			{
+				tokens: themeConfig.spacing,
+				searchValue: '--mantine-spacing',
+			},
+			{
+				tokens: themeConfig.screens,
+				searchValue: '--mantine-breakpoint',
+			},
+			{
+				tokens: themeConfig.borderRadius,
+				searchValue: '--mantine-radius-default',
+			},
+			{
+				tokens: themeConfig.borderRadius,
+				searchValue: '--mantine-radius',
+			},
+			{
+				tokens: themeConfig.boxShadow,
+				searchValue: '--mantine-shadow',
+			},
+		]
+
+		for (const key in resolvedVariables) {
+			const variables = resolvedVariables[key as keyof typeof resolvedVariables]
+
+			for (const variable in variables) {
+				const tokenNameConfig = tokenNameConfigs.find((config) =>
+					variable.startsWith(config.searchValue),
+				)
+
+				if (tokenNameConfig) {
+					const tokenName = variable.replace(
+						tokenNameConfig.searchValue,
+						tokenNameConfig.replaceValue ?? '',
+					)
+
+					const tokenNameWithPrefix =
+						tokenName === '' ? prefix : prefix + tokenName
+
+					tokenNameConfig.tokens[tokenNameWithPrefix] = `var(${variable})`
 				}
-
-				// Variant colors
-				const variantColorNames = [
-					// Filled variant
-					'filled',
-					'filled-hover',
-					// Light variant
-					'light',
-					'light-hover',
-					'light-color',
-					// Outline variant
-					'outline',
-					'outline-hover',
-				]
-
-				for (const variantColorName of variantColorNames) {
-					colors[`${prefix}-${colorName}-${variantColorName}`] =
-						`var(--mantine-color-${colorName}-${variantColorName})`
-				}
 			}
-
-			// Primary color variables
-			for (let shade = 0; shade < 10; shade++) {
-				colors[`${prefix}-primary-${shade}`] =
-					`var(--mantine-primary-color-${shade})`
-			}
-
-			colors[`${prefix}-primary-contrast`] =
-				'var(--mantine-primary-color-contrast)'
-
-			const primaryColorNames = [
-				'primary-filled',
-				'primary-filled-hover',
-				'primary-light',
-				'primary-light-hover',
-				'primary-light-color',
-			]
-
-			for (const colorName of primaryColorNames) {
-				colors[`${prefix}-${colorName}`] = `var(--mantine-color-${colorName})`
-			}
-
-			// Other color variables
-			const otherColorNames = [
-				'white',
-				'black',
-				'text',
-				'body',
-				'error',
-				'placeholder',
-				'dimmed',
-				'bright',
-				'anchor',
-				'default',
-				'default-hover',
-				'default-color',
-				'default-border',
-			]
-
-			for (const colorName of otherColorNames) {
-				colors[`${prefix}-${colorName}`] = `var(--mantine-color-${colorName})`
-			}
-		}
-
-		// Spacing variables
-		const spacing: KeyValuePair = {}
-
-		for (const size in theme.spacing) {
-			spacing[`${prefix}-${size}`] = `var(--mantine-spacing-${size})`
-		}
-
-		// Breakpoints variables
-		const screens: KeyValuePair = {}
-
-		for (const size in theme.breakpoints) {
-			screens[`${prefix}-${size}`] = `var(--mantine-breakpoint-${size})`
-		}
-
-		// Border radius variables
-		const borderRadius: KeyValuePair = {
-			[`${prefix}`]: `var(--mantine-radius-${theme.defaultRadius})`,
-		}
-
-		for (const size in theme.radius) {
-			borderRadius[`${prefix}-${size}`] = `var(--mantine-radius-${size})`
-		}
-
-		// Shadow variables
-		const boxShadow: KeyValuePair = {}
-
-		for (const size in theme.shadows) {
-			boxShadow[`${prefix}-${size}`] = `var(--mantine-shadow-${size})`
-		}
-
-		// z-index variables
-		const zIndexLevels = ['app', 'modal', 'popover', 'overlay', 'max']
-
-		const zIndex: KeyValuePair = {}
-
-		for (const level of zIndexLevels) {
-			zIndex[`${prefix}-${level}`] = `var(--mantine-z-index-${level})`
 		}
 
 		return {
 			theme: {
-				extend: {
-					cursor,
-					fontFamily,
-					fontSize,
-					lineHeight,
-					fontWeight,
-					colors,
-					spacing,
-					screens,
-					borderRadius,
-					boxShadow,
-					zIndex,
-				},
+				extend: themeConfig,
 			},
 		}
 	},
