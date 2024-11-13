7d1ce5553b2230d32144afcbcec1b634e090409b
Visualizeit <17688172112@163.com>
2024-11-13 23:22:34 +0800

    Enhance UtilitiesTable to display computed CSS property values with HoverCard

--- pages/index/components/UtilitiesTable.tsx
+++ pages/index/components/UtilitiesTable.tsx
@@ -1,4 +1,4 @@
-import { Code, Table } from '@mantine/core'
+import { Table, Code, HoverCard, ColorSwatch, Group } from '@mantine/core'
 
 interface UtilitiesTableProps {
 	classPrefix: string
@@ -15,20 +15,47 @@ const UtilitiesTable = ({ classPrefix, utilities }: UtilitiesTableProps) => {
 				</Table.Tr>
 			</Table.Thead>
 			<Table.Tbody>
-				{utilities.map(([className, properties]) => (
-					<Table.Tr key={className}>
-						<Table.Td>
-							<Code unstyled>
-								{classPrefix ? `${classPrefix}-${className}` : className}
-							</Code>
-						</Table.Td>
-						<Table.Td>
-							<Code unstyled>
-								{Array.isArray(properties) ? properties[0] : properties}
-							</Code>
-						</Table.Td>
-					</Table.Tr>
-				))}
+				{utilities.map(([className, properties]) => {
+					const property = (
+						Array.isArray(properties) ? properties[0] : properties
+					)
+						.replace('var(', '')
+						.replace(')', '')
+
+					const propertyValue =
+						typeof window === 'undefined'
+							? null
+							: window
+									.getComputedStyle(document.documentElement)
+									.getPropertyValue(property)
+
+					return (
+						<Table.Tr key={className}>
+							<Table.Td>
+								<Code unstyled>
+									{classPrefix ? `${classPrefix}-${className}` : className}
+								</Code>
+							</Table.Td>
+							<Table.Td>
+								<HoverCard position="right">
+									<HoverCard.Target>
+										<Code unstyled>{property}</Code>
+									</HoverCard.Target>
+									<HoverCard.Dropdown>
+										{propertyValue && (
+											<Group className="max-w-72" align="center">
+												<Code unstyled>{propertyValue}</Code>
+												{property.includes('color') && (
+													<ColorSwatch size={20} color={propertyValue} />
+												)}
+											</Group>
+										)}
+									</HoverCard.Dropdown>
+								</HoverCard>
+							</Table.Td>
+						</Table.Tr>
+					)
+				})}
 			</Table.Tbody>
 		</Table>
 	)
