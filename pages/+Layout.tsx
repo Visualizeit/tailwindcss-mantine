4b4a530f777f575e0f426bacdd353513c5109fff
Visualizeit <17688172112@163.com>
2024-11-17 21:59:18 +0800

    Update dependencies to use @mantinex/shiki for code highlighting and enhance layout structure

--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -1,15 +1,26 @@
 import { MantineProvider } from '@mantine/core'
+import { ShikiProvider } from '@mantinex/shiki'
 import { type PropsWithChildren } from 'react'
 import mantineTheme from '../helpers/mantineTheme'
 
 import '@mantine/core/styles.css'
-import '@mantine/code-highlight/styles.css'
+import '@mantinex/shiki/styles.css'
 import './index.css'
+
+const loadShiki = async () => {
+	const { createHighlighter } = await import('shiki')
+
+	return createHighlighter({
+		themes: [],
+		langs: ['bash', 'javascript'],
+	})
+}
 
 export const Layout = ({ children }: PropsWithChildren<{}>) => {
 	return (
 		<MantineProvider defaultColorScheme="auto" theme={mantineTheme}>
-			{children}
+			{/* @ts-expect-error */}
+			<ShikiProvider loadShiki={loadShiki}>{children}</ShikiProvider>
 		</MantineProvider>
 	)
 }
